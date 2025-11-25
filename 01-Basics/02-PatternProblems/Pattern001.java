9e76145fe5d57b3207b266d11d3aad3e83219ade
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2025-11-25 19:14:04 +0530

    Update Pattern001.java

--- 01-Basics/02-PatternProblems/Pattern001.java
+++ 01-Basics/02-PatternProblems/Pattern001.java
@@ -7,7 +7,7 @@ Pattern:
 * * * * *
 */
 
-public class Pattern1 {
+public class Pattern001 {
     public static void main(String[] args){
         for(int i=1; i<=5; i++) {
             for(int j=1; j<=5; j++) {
