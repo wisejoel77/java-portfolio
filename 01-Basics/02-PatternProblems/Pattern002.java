7a57055d26054a0e61faeef969f3f2100ca7ee63
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2025-11-25 19:14:17 +0530

    Update Pattern002.java

--- 01-Basics/02-PatternProblems/Pattern002.java
+++ 01-Basics/02-PatternProblems/Pattern002.java
@@ -7,7 +7,7 @@ Pattern:
 * * * * *
 */
 
-public class Pattern2 {
+public class Pattern002 {
     public static void main(String[] args){
         for(int i=1; i<=5; i++){
             for(int j=1; j<=i; j++){
@@ -17,3 +17,4 @@ public class Pattern2 {
         }
     }
 }
+
