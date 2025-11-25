eb772cefbd06c3cddc5f4e23d9f049b370c00040
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2025-11-25 19:10:36 +0530

    Add files via upload

---
+++ 01-Basics/02-PatternProblems/Pattern7.java
@@ -0,0 +1,35 @@
+/*
+Pattern:
+*
+* *
+* * *
+* * * *
+* * *
+* *
+*
+*/
+
+import java.util.Scanner;
+
+public class Pattern7 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+        int a = 0;
+        for(int i=1; i<=((rows/2)+1); i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            a = i;
+        }
+        for(int i=a+1; i<=rows; i++){
+            for(int j=1; j<=((rows-i)+1); j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
