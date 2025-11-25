8a22aa6da79d830f3cfb810c3f35f0728ca7ae19
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2025-11-25 19:11:01 +0530

    Add files via upload

---
+++ 01-Basics/02-PatternProblems/Pattern9.java
@@ -0,0 +1,29 @@
+/*
+Pattern:
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+*/
+
+import java.util.Scanner;
+
+public class Pattern9 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.println();
+        for(int i=rows; i>=1; i--){
+            for(int j=1; j<=(rows-i); j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=((i*2)-1); j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
