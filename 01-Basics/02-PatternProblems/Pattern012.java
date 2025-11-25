7ab9e7f04d51eef2daae2f855c65081390803680
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2025-11-25 19:16:55 +0530

    Add files via upload

---
+++ 01-Basics/02-PatternProblems/Pattern012.java
@@ -0,0 +1,32 @@
+/*
+Pattern:(Hollow Rectangle)
+* * * * * * *
+*           *
+*           *
+*           *
+* * * * * * *
+*/
+
+import java.util.Scanner;
+
+public class Pattern12 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = scanner.nextInt();
+        System.out.println();
+        for(int i=1; i<=rows; i++){
+            for(int j=1; j<=cols; j++){
+                if(i==1 || i==rows || j==1 || j==cols){
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
