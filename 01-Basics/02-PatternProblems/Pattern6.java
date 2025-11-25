a450d784877bfce7700b3a592f0dea32023700b0
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2025-11-25 19:10:24 +0530

    Add files via upload

---
+++ 01-Basics/02-PatternProblems/Pattern6.java
@@ -0,0 +1,28 @@
+/*
+Pattern:
+* * * * *
+  * * * *
+    * * *
+      * *
+        *
+*/
+
+import java.util.Scanner;
+
+public class Pattern6 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+        for(int i=rows; i>=1; i--) {
+            for(int j=1; j<=(rows-i); j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
