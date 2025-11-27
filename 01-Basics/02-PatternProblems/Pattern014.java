5c2ef0b833c921bdf5b780b8246ad4d94ddbbfec
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2025-11-27 07:48:23 +0530

    Update Pattern014.java

--- 01-Basics/02-PatternProblems/Pattern014.java
+++ 01-Basics/02-PatternProblems/Pattern014.java
@@ -16,7 +16,7 @@ public class Pattern014 {
         int rows = scanner.nextInt();
         for(int i=1; i<=rows; i++){
             for(int j=1; j<=(rows-i); j++){
-                System.out.print("  ");
+                System.out.print(" ");
             }
             for(int j=1; j<=i; j++){
                 System.out.print("* ");
@@ -25,4 +25,5 @@ public class Pattern014 {
         }
         scanner.close();
     }
+
 }
