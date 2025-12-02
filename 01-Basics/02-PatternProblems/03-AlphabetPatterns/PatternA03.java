e697ede553c53d38933a9e642a8c6f22e72cc5e5
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2025-12-02 15:40:46 +0530

    Add files via upload

---
+++ 01-Basics/02-PatternProblems/03-AlphabetPatterns/PatternA03.java
@@ -0,0 +1,30 @@
+/*
+Pattern:
+A
+B B
+C C C
+D D D D
+E E E E E
+*/
+
+import java.util.Scanner;
+
+public class PatternA03 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+        char ch = 'A';
+        for(int i=1; i<=rows; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(ch);
+                if(j<i){
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            ch ++;
+        }
+        scanner.close();
+    }
+}
