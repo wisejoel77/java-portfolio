320e28ca1cbf5ef07541d9450dd6af2696828996
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2026-07-06 21:24:04 +0530

    Create Main.java

---
+++ 02-OOPS/05-OtherConcepts/Composition/Main.java
@@ -0,0 +1,8 @@
+package com.wisejoel77.composition;
+
+public class Main {
+    public static void main(String[] args){
+        Person person = new Person("Arnold Joel Joshua Matangi", 20, "India", "Andhra Pradesh", "Nuzvid", 521202);
+        person.getDetails();
+    }
+}
