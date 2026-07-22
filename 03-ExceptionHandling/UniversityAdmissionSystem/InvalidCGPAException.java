1a6f0e8514028e01498fab554b4723876e3df5e9
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2026-07-22 16:06:07 +0530

    Create InvalidCGPAException.java

---
+++ 03-ExceptionHandling/UniversityAdmissionSystem/InvalidCGPAException.java
@@ -0,0 +1,8 @@
+package com.wisejoel77.ExceptionHandling.UniversityAdmissionSystem;
+
+public class InvalidCGPAException extends Exception{
+    // Constructor
+    public InvalidCGPAException(String message){
+        super(message);
+    }
+}
